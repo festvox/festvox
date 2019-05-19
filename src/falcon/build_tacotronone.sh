b8b13380bab7e1ff86a3ad5fd8547be8d313cc54
Sai Krishna <srallaba@cs.cmu.edu>
2019-05-19 12:39:53 +0100

    Update build_tacotronone.sh

--- src/falcon/build_tacotronone.sh
+++ src/falcon/build_tacotronone.sh
@@ -6,3 +6,5 @@ python3.5 $FALCONDIR/prepare_data.py etc/txt.done.data data/dataprep_tacotron1 w
 ### Train Baseline Model
 python3.5 $FALCONDIR/train_tacotronone.py --data-root data/dataprep_tacotron1 --checkpoint-dir checkpoints > log_tacotronone
 
+### Test Baseline Model
+python3.5 $FALCONDIR/synthesize_tacotronone.py checkpoints/checkpoint_step70000.pth data/dataprep_tacotron1/test.txt test/tacotronone
