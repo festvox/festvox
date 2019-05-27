2eb0d7b3e524d8e27e97ab6a740aa232c2087453
Sai Krishna <srallaba@cs.cmu.edu>
2019-05-27 15:10:15 -0400

    Update build_tacotronone.sh

--- src/falcon/build_tacotronone.sh
+++ src/falcon/build_tacotronone.sh
@@ -2,9 +2,8 @@
 export FALCONDIR=$FESTVOXDIR/src/falcon
 
 ### Prepare data
-cat etc/txt.done.data | tr '(' ' ' | tr ')' ' ' | tr '"' ' ' > etc/txt.done.data.tacotron
-mkdir -p data/dataprep_tacotron1
-python3.5 $FALCONDIR/prepare_data.py etc/txt.done.data data/dataprep_tacotron1 wav
+cat etc/txt.done.data | tr '(' ' ' | tr ')' ' ' | tr '"' ' ' > etc/tddmkdir -p data/dataprep_tacotron1
+python3.5 $FALCONDIR/prepare_data.py etc/tdd data/dataprep_tacotron1 wav
 
 
 ### Train Baseline Model
