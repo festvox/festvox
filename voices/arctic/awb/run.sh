e817526fba438d0019276982188c3e4f2d4a2f07
saikrishnarallabandi <srallaba@cs.cmu.edu>
2019-12-31 19:18:59 +0000

    Added run.sh in awb

--- voices/arctic/awb/run.sh
+++ voices/arctic/awb/run.sh
@@ -10,5 +10,10 @@ cp /home_original/srallaba/data/arctic/cmu_us_${spk}_arctic/etc/txt.done.data et
 
 ./bin/do_build build_prompts || exit 1
 ./bin/do_build get_phseq || exit 1
+cd ..
 
+python3.5 $FALCONDIR/utils/dataprep_addphones.py ${VOXDIR}/ehmm/etc/txt.phseq.data ${VOXDIR}
+cat ${VOXDIR}/ehmm/etc/txt.phseq.data | awk '{print $1}' > ${VOXDIR}/fnames
+python3.5 $FALCONDIR/utils/dataprep_addlspec.py ${VOXDIR}/fnames ${VOXDIR}
+python3.5 $FALCONDIR/utils/dataprep_addmspec.py ${VOXDIR}/fnames ${VOXDIR}
 
