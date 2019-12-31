d304052a3d4e647f872fff0ee75de023e7af4b3c
saikrishnarallabandi <srallaba@cs.cmu.edu>
2019-12-31 18:59:01 +0000

    Added get_phseq to clustergen

--- voices/arctic/awb/run.sh
+++ voices/arctic/awb/run.sh
@@ -9,6 +9,6 @@ cp /home_original/srallaba/data/arctic/cmu_us_${spk}_arctic/etc/txt.done.data et
 ./bin/get_wavs /home_original/srallaba/data/arctic/cmu_us_${spk}_arctic/wav/*
 
 ./bin/do_build build_prompts || exit 1
-./bin/do_build label || exit 1
+./bin/do_build get_phseq || exit 1
 
 
