b2d9519bb1e66b4e4f52cc86c675ca2d733ba396
saikrishnarallabandi <srallaba@cs.cmu.edu>
2020-01-01 18:56:45 +0000

    Added generic stuff to awb

--- voices/arctic/awb/run.sh
+++ voices/arctic/awb/run.sh
@@ -5,8 +5,8 @@ mkdir -p ${VOXDIR}
 cd ${VOXDIR}
 $FESTVOXDIR/src/clustergen/setup_cg cmu us ${spk} || exit 1
 
-cp /home_original/srallaba/data/arctic/cmu_us_${spk}_arctic/etc/txt.done.data etc/
-./bin/get_wavs /home_original/srallaba/data/arctic/cmu_us_${spk}_arctic/wav/*
+cp ${data_dir}/arctic/cmu_us_${spk}_arctic/etc/txt.done.data etc/
+./bin/get_wavs ${data_dir}/arctic/cmu_us_${spk}_arctic/wav/*
 
 ./bin/do_build build_prompts || exit 1
 ./bin/do_build get_phseq || exit 1
