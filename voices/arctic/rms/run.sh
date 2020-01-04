ebcde67aec2bebe51126460c610d318b69b08712
saikrishnarallabandi <srallaba@cs.cmu.edu>
2020-01-04 17:57:59 +0000

    Added final frame stuff to blocks

--- voices/arctic/rms/run.sh
+++ voices/arctic/rms/run.sh
@@ -1,15 +1,14 @@
+VOXDIR=vox
+spk=awb
 
 source path.sh
-
-VOXDIR=vox
-spk=rms
 
 mkdir -p ${VOXDIR}
 cd ${VOXDIR}
 $FESTVOXDIR/src/clustergen/setup_cg cmu us ${spk} || exit 1
 
-cp ${data_dir}/arctic/cmu_us_${spk}_arctic/etc/txt.done.data etc/
-./bin/get_wavs ${data_dir}/arctic/cmu_us_${spk}_arctic/wav/*
+cp ${data_dir}/arctic/cmu_us_${spk}_arctic/etc/txt.done.data etc/ || exit 1
+./bin/get_wavs ${data_dir}/arctic/cmu_us_${spk}_arctic/wav/* || exit 1
 
 ./bin/do_build build_prompts || exit 1
 ./bin/do_build get_phseq || exit 1
@@ -23,4 +22,5 @@ python3.5 $FALCONDIR/utils/dataprep_addmspec.py ${VOXDIR}/fnames ${VOXDIR}
 ${VOXDIR}/bin/traintest ${VOXDIR}/fnames 
 cp ${VOXDIR}/fnames.test ${VOXDIR}/fnames.val
 
-python3.5 local/train_phones.py --exp-dir exp/taco_one_phseq --conf conf/tacotron.conf
+python3.5 local/train_phones.py --exp-dir exp/taco_one_phseq 
+python3.5 local/synthesize_phones.py exp/taco_one_phseq/checkpoints/checkpoint_step30000.pth  vox/ehmm/etc/txt.phseq.data.test.head exp/taco_one_phseq/tts_phseq
