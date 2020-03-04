bed3d2d2fba5074008124d33904132f46abf2949
saikrishnarallabandi <srallaba@cs.cmu.edu>
2020-03-04 03:53:34 -0500

    Added dumping kaldi mfccs

---
+++ challenges/msrlid2020/partA/dump_mfccs.sh
@@ -0,0 +1,22 @@
+# Source the kaldi path
+mfcc_folder=$1
+feats_dir=$2
+
+rm -r ${feats_dir}
+
+mkdir -p ${feats_dir}/cleaned
+mkdir -p ${feats_dir}/raw
+
+# Get the mfccs and accomodate in a single file
+for file in ${mfcc_folder}/raw_*.ark
+do
+ fname=$(basename "$file" .ark)
+ cat ${mfcc_folder}/${fname}.scp | while read f
+ do
+   n=`echo "${f}" | cut -d ' ' -f 1`
+   echo $f | copy-feats scp:- ark,t:- | add-deltas ark:- ark,t:- > ${feats_dir}/raw/${n}.mfcc # | apply-cmvn scp:${mfcc_folder}/../cmvn_${lang}.scp ark:- ark,t:- > ../data/${lang}/raw/${n}.mfcc
+   #echo $f | copy-feats scp:- ark,t:- | add-deltas ark:- ark,t:-  | apply-cmvn scp:${mfcc_folder}/../data/train/cmvn.scp scp:${mfcc_folder}/../data/test/cmvn.scp  ark:- ark,t:- > ../feats/raw/${n}.mfcc
+   cat ${feats_dir}/raw/${n}.mfcc | sed '/\[$/d' | sed 's/]//g' > ${feats_dir}/cleaned/${n}.mfcc
+ done
+done
+
