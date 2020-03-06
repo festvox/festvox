48ec62832858fe845d35682b2bc9ae8c0a92068a
Sai Krishna <srallaba@cs.cmu.edu>
2020-03-06 09:37:46 -0500

    Update dump_mfccs.sh

--- challenges/msrlid2020/partA/dump_mfccs.sh
+++ challenges/msrlid2020/partA/dump_mfccs.sh
@@ -14,9 +14,8 @@ do
  cat ${mfcc_folder}/${fname}.scp | while read f
  do
    n=`echo "${f}" | cut -d ' ' -f 1`
-   echo $f | copy-feats scp:- ark,t:- | add-deltas ark:- ark,t:- > ${feats_dir}/raw/${n}.mfcc # | apply-cmvn scp:${mfcc_folder}/../cmvn_${lang}.scp ark:- ark,t:- > ../data/${lang}/raw/${n}.mfcc
+   echo $f | copy-feats scp:- ark,t:- | apply-cmvn --norm-vars=true --utt2spk=ark:data/train/utt2spk scp:${mfcc_folder}/cmvn_train.scp ark:- ark,t:- | add-deltas ark:- ark,t:- > ${feats_dir}/raw/${n}.mfcc # | apply-cmvn scp:${mfcc_folder}/../cmvn_${lang}.scp ark:- ark,t:- > ../data/${lang}/raw/${n}.mfcc
    #echo $f | copy-feats scp:- ark,t:- | add-deltas ark:- ark,t:-  | apply-cmvn scp:${mfcc_folder}/../data/train/cmvn.scp scp:${mfcc_folder}/../data/test/cmvn.scp  ark:- ark,t:- > ../feats/raw/${n}.mfcc
    cat ${feats_dir}/raw/${n}.mfcc | sed '/\[$/d' | sed 's/]//g' > ${feats_dir}/cleaned/${n}.mfcc
  done
 done
-
