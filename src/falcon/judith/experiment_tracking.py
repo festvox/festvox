3dc6e196b6b5002c346cb014d405ef52b270337d
saikrishnarallabandi <srallaba@cs.cmu.edu>
2020-07-28 16:28:27 +0000

    Cleaned falcon stuff

---
+++ src/falcon/judith/experiment_tracking.py
@@ -0,0 +1,21 @@
+import os, sys
+import glob
+import config
+import json
+import neptune
+
+projects_dir = 'sandbox'
+
+class RemoteTracker(object):
+
+    def __init__(self, exp_name, projects_dir='sandbox'):
+       self.default_project = project_name
+       self.tracker = neptune
+       self.tracker.init('srallaba/' + projects_dir)
+       self.tracker.create_experiment(name=exp_name,
+                              abort_callback=lambda: run_shutdown_logic_and_exit())
+
+    def log_scalar(self, name, val):
+       self.tracker.log_metric(name, val)
+
+
