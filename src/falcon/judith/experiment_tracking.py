2411039cc48dbf51273547202c9a5f65b36e4c21
saikrishnarallabandi <srallaba@cs.cmu.edu>
2020-07-28 16:57:22 +0000

    Added judith using neptune to falcon

--- src/falcon/judith/experiment_tracking.py
+++ src/falcon/judith/experiment_tracking.py
@@ -1,6 +1,5 @@
 import os, sys
 import glob
-import config
 import json
 import neptune
 
@@ -9,11 +8,11 @@ projects_dir = 'sandbox'
 class RemoteTracker(object):
 
     def __init__(self, exp_name, projects_dir='sandbox'):
-       self.default_project = project_name
        self.tracker = neptune
        self.tracker.init('srallaba/' + projects_dir)
        self.tracker.create_experiment(name=exp_name,
                               abort_callback=lambda: run_shutdown_logic_and_exit())
+       print("Initialized Tracker") 
 
     def log_scalar(self, name, val):
        self.tracker.log_metric(name, val)
