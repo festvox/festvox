a1c8a79f4d7deae58943b0e6e157b39648edbe09
Sai Krishna <srallaba.research@gmail.com>
2026-03-03 07:13:40 -0600

    Make train_phones scripts device-aware for CPU/GPU

--- voices/multispeaker/vbasic_arctic/local/train_phones.py
+++ voices/multispeaker/vbasic_arctic/local/train_phones.py
@@ -57,6 +57,7 @@ vox_dir ='vox'
 global_step = 0
 global_epoch = 0
 use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
 if use_cuda:
     cudnn.benchmark = False
 use_multigpu = None
@@ -71,8 +72,7 @@ def train(model, train_loader, val_loader, optimizer,
           checkpoint_dir=None, checkpoint_interval=None, nepochs=None,
           clip_thresh=1.0):
     model.train()
-    if use_cuda:
-        model = model.cuda()
+    model = model.to(device)
     linear_dim = model.linear_dim
 
     criterion = nn.L1Loss()
@@ -99,8 +99,7 @@ def train(model, train_loader, val_loader, optimizer,
 
             # Feed data
             x, spk, mel, y = Variable(x), Variable(spk), Variable(mel), Variable(y)
-            if use_cuda:
-                x, spk, mel, y = x.cuda(), spk.cuda(), mel.cuda(), y.cuda()
+            x, spk, mel, y = x.to(device), spk.to(device), mel.to(device), y.to(device)
 
             # Multi GPU Configuration
             if use_multigpu:
@@ -236,7 +235,7 @@ if __name__ == "__main__":
                      padding_idx=hparams.padding_idx,
                      use_memory_mask=hparams.use_memory_mask,
                      )
-    model = model.cuda()
+    model = model.to(device)
     #model = DataParallelFix(model)
 
     optimizer = optim.Adam(model.parameters(),
@@ -247,7 +246,7 @@ if __name__ == "__main__":
     # Load checkpoint
     if checkpoint_path:
         print("Load checkpoint from: {}".format(checkpoint_path))
-        checkpoint = torch.load(checkpoint_path)
+        checkpoint = torch.load(checkpoint_path, map_location=device)
         model.load_state_dict(checkpoint["state_dict"])
         optimizer.load_state_dict(checkpoint["optimizer"])
         try:
